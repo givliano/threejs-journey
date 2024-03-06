525b46e81cfca59de7be1d26ed39094360b8f1c3
Giuliano Machado <giuliano.machado@mindera.com>
2024-03-06 11:01:28 +0000

    finished code

--- 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main()
 {
     // Disc pattern
@@ -21,6 +23,9 @@ void main()
     strength = 1.0 - strength;
     strength = pow(strength, 10.0);
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    // Final color
+    vec3 finalColor = mix(vec3(0.0), vColor, strength);
+
+    gl_FragColor = vec4(finalColor, 1.0);
     #include <colorspace_fragment>;
 }
--- 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -1,12 +1,37 @@
 uniform float uSize;
+uniform float uTime;
 
 attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
 
 void main()
 {
+    // POSITION
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // SPIN (works because galaxy is at center of the scene)
+    // We calculate the particle angle and its distance to the center
+    // We increase the angle according to the `uTime` and the distance
+    // We update the position according to that new angle
+
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    // makes the further particles from the center move slower than
+    // the ones close to the center
+    // uTime is too strong so we make it lower by the multiplication
+    float angleOffset = (1.0 / distanceToCenter) * (uTime * 0.2);
+    angle += angleOffset;
+    modelPosition.x = cos(angle) * distanceToCenter;
+    modelPosition.z = sin(angle) * distanceToCenter;
+
+    // Randomness
+    modelPosition.xyz += aRandomness;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
+    modelPosition.y += uTime;
     gl_Position = projectionPosition;
 
     // SIZE
@@ -16,4 +41,8 @@ void main()
     // 1.0 is the `scale`
     // `viewPosition` is the mvPosition aka movelViewPosition
     gl_PointSize *= (1.0 / - viewPosition.z);
-}
+
+    // COLOR
+    vColor = color;
+
+  }
