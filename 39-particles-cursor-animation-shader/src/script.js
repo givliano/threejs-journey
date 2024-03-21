6e116f768a3ec3119fc37b4c8f927216217cfe9f
Giuliano Machado <giuliano.machado@mindera.com>
2024-03-21 18:19:01 +0000

    add code to make particles animate over image

--- 39-particles-cursor-animation-shader/src/script.js
+++ 39-particles-cursor-animation-shader/src/script.js
@@ -98,10 +98,54 @@ displacement.glowImage.src = './glow.png';
 displacement.glowImage.onload = () => {
     displacement.context.drawImage(displacement.glowImage, 20, 20, 32, 32); // x, y, width, height
 }
+
+// Interactive plane
+// since raycaster doesn't work in particles we need a plane behind
+// the particles for the effect
+// we wont reuse the plane from the particles because it has too many
+// vertices and that makes it bad for performance with the `raycaster`
+// but it has to be the same size
+displacement.interactivePlane = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+);
+displacement.interactivePlane.visible = false;
+scene.add(displacement.interactivePlane);
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster();
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999); // move the cursor to outside the screen
+displacement.canvasCursor = new THREE.Vector2(9999, 9999); // move the cursor to outside the screen
+
+// same as mousemove but works in mobile
+window.addEventListener('pointermove', (e) => {
+    // we must convert the screen cordinates (which are in `pixels`) to the `clip space`,
+    // which have coordinates from `- 1` to `+ 1` (left to right, bottom to top)
+    displacement.screenCursor.x = ( e.clientX / sizes.width ) * 2 - 1;
+    displacement.screenCursor.y = - ( e.clientY / sizes.height ) * 2 + 1;
+})
+
+// Texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas);
+
+
 /**
  * Particles
  */
 const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
+
+const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count);
+const anglesArray = new Float32Array(particlesGeometry.attributes.position.count);
+
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+    intensitiesArray[i] = Math.random();
+    anglesArray[i] = Math.random() * Math.PI * 2; // position randomly around a circle coordinate
+}
+
+particlesGeometry.setAttribute('aIntensity', new THREE.BufferAttribute(intensitiesArray, 1));
+particlesGeometry.setAttribute('aAngle', new THREE.BufferAttribute(intensitiesArray, 1));
 
 const particlesMaterial = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
@@ -109,7 +153,8 @@ const particlesMaterial = new THREE.ShaderMaterial({
     uniforms:
     {
         uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-        uPictureTexture: new THREE.Uniform(textureLoader.load('./picture-1.png')) // any picture, small, grayscale and square
+        uPictureTexture: new THREE.Uniform(textureLoader.load('./picture-1.png')), // any picture, small, grayscale and square
+        uDisplacementTexture: new THREE.Uniform(displacement.texture),
     }
 })
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
@@ -122,6 +167,54 @@ const tick = () =>
 {
     // Update controls
     controls.update()
+
+    /**
+      * Raycaster
+      */ 
+    displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
+    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane);
+
+    if (intersections.length) {
+        const uv = intersections[0].uv;
+
+        displacement.canvasCursor.x = uv.x * displacement.canvas.width; // from 0 to 128
+        // Invert the Y because canvas goes from 0 at the top to 1 at the bottom
+        // but the uv goes from 0 at the bottom to 1 at the top
+        displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height; // from 0 to 128
+    }
+
+    /**
+      * Displacement
+      */ 
+    // FADE OUT
+    // Fill the whole canvas with a black but low opacity. That's the way to do a fade out
+    // in canvas
+    displacement.context.globalCompositeOperation = 'source-over'; // set the composition to the default one
+    // There is a problem with Canvas handling small numbers getting close to 0 and the small variations
+    // in color
+    // it will be fixed when using the canvas data inside webGL
+    displacement.context.globalAlpha = 0.02;
+    displacement.context.fillRect(
+        0, 
+        0, 
+        displacement.canvas.width, 
+        displacement.canvas.height
+    );
+    // Draw glow
+    const glowSize = displacement.canvas.width * 0.25;
+    displacement.context.globalCompositeOperation = 'lighten'; // retains the lightest pixels of the layers
+    displacement.context.globalAlpha = 1;
+    displacement.context.drawImage(
+        displacement.glowImage,
+        displacement.canvasCursor.x - glowSize * 0.5, // move the center of the glow to the pointer location
+        displacement.canvasCursor.y - glowSize * 0.5,
+        glowSize, // width
+        glowSize  // height
+    );
+
+    // Texture
+    // warns Three.JS to send the buffer data to the GPU
+    displacement.texture.needsUpdate = true;
 
     // Render
     renderer.render(scene, camera)
--- 39-particles-cursor-animation-shader/src/shaders/particles/vertex.glsl
+++ 39-particles-cursor-animation-shader/src/shaders/particles/vertex.glsl
@@ -1,13 +1,39 @@
 uniform vec2 uResolution;
 uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+
+attribute float aIntensity;
+attribute float aAngle;
 
 varying vec3 vColor;
 
 // Here a vertex is each particle. A particle is consisted of many different fragments (the `pixels`);
 void main()
 {
+    // Displacement
+    vec3 newPosition = position;
+    float displacementIntensity = texture(uDisplacementTexture, uv).r;
+    // Ignore everything below 0.1 to avoid the bug from canvas fade off never going full to 0.0
+    displacementIntensity = smoothstep(0.1, 1.0, displacementIntensity);
+
+    // move them on the xyz axes
+    vec3 displacement = vec3(
+        // we send the same angle as `sin` and `cos` to get a circle
+        cos(aAngle) * 0.2,
+        sin(aAngle) * 0.2,
+        1.0
+    );
+    displacement = normalize(displacement);
+    // If the pixel is black the dispalcement is 0.0
+    // if the pixel is white the displacement is 1.0
+    displacement *= displacementIntensity;
+    displacement *= 3.0; // makes the effect bigger
+    displacement *= aIntensity;
+
+    newPosition += displacement;
+
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
