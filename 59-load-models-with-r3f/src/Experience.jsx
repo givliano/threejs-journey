cb7633113c79b431caef0bfddba8c0394cd34563
Giuliano Machado <giuliano.machado@mindera.com>
2024-04-22 20:12:51 +0100

    add 59 initial code

---
+++ 59-load-models-with-r3f/src/Experience.jsx
@@ -0,0 +1,36 @@
+import { OrbitControls } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Perf } from 'r3f-perf'
+
+export default function Experience()
+{
+    const model = useLoader(GLTFLoader, './hamburger.glb');
+    console.log(model);
+
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh castShadow position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
