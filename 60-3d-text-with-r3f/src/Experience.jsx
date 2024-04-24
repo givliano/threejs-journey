e07f8a58374f5b58340eb1c86f94d69b32acd84f
Giuliano Machado <giuliano.machado@mindera.com>
2024-04-24 12:24:13 +0100

    add finished 61 code

--- 60-3d-text-with-r3f/src/Experience.jsx
+++ 60-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,51 @@
+import { useState, useEffect, useRef } from 'react';
 import { Text3D, OrbitControls, Center, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber';
+
+const torusGeometry = new THREE.TorusGeometry();
+const material = new THREE.MeshMatcapMaterial();
 
 export default function Experience()
 {
-    const matcapTexture = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
-    console.log(matcapTexture);
+    const donuts = useRef([]);
+    // const donutsGroup = useRef();
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+    // NOTE outdated way of adding geometries
+    // const [ torusGeometry, seTorusGeometry ] = useState();
+    // const [ material, setMaterial ] = useState();
+
+    useEffect(() => {
+        // NOTE Change the encodnig back after creating it with
+        // the default one outside React
+        material.encoding = THREE.sRGBEncoding;
+        material.needsUpdate = true; // NOTE force the update;
+
+        material.matcap = matcapTexture;
+        material.needsUpdate = true; // NOTE force the update;
+    }, [])
+
+    useFrame((_, delta) => {
+        for (const donut of donuts.current) {
+            if (donut) {
+                donut.rotation.y += delta * 0.2;
+            }
+        }
+    })
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
+        {/* <torusGeometry ref={ seTorusGeometry } /> */}
+        {/* <meshMatcapMaterial ref={ setMaterial } matcap={ matcapTexture } /> */}
+
         <Center>
             <Text3D 
+                material={ material }
                 font='./fonts/helvetiker_regular.typeface.json'
                 size={ 0.75 }
                 height={ 0.2 }
@@ -24,8 +57,29 @@ export default function Experience()
                 bevelSegments={ 5 }
             >
                 Hello R3F
-                <meshNormalMaterial />
             </Text3D>
         </Center>
+
+        {/* <group> */}
+            { [ ...Array(100) ].map((_, i) => (
+                <mesh
+                    ref={ (element) => donuts.current[i] = element /**avoid adding infinitely donuts**/}
+                    key={ i }
+                    geometry={ torusGeometry }
+                    material={ material }
+                    position={[
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                    ]}
+                    scale={ 0.2 + (Math.random() * 0.2) }
+                    rotation={[
+                        Math.random() * Math.PI,
+                        Math.random() * Math.PI,
+                        0
+                    ]}
+                />
+            ))}
+        {/* </group> */}
     </>
 }
