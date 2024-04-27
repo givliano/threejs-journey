884f91593c732fc70c33a7e5a5cc148f9e186a81
Giuliano Machado <giuliano.machado@mindera.com>
2024-04-27 17:47:27 +0100

    add initial 64 code

---
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -0,0 +1,17 @@
+import { OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+    return <>
+
+        <color args={[ '#241a1a' ]} attach="background" />
+
+        <OrbitControls makeDefault />
+        
+        <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+
+    </>
+}
