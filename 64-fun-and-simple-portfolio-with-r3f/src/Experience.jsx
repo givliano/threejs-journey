9c15e6d58d25324735ca2493e859636b794a79fa
Giuliano Machado <giuliano.machado@mindera.com>
2024-04-29 12:08:26 +0100

    finished code 64

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,17 +1,72 @@
-import { OrbitControls } from '@react-three/drei'
+import { 
+    ContactShadows,
+    Environment,
+    Float,
+    Html,
+    Text,
+    PresentationControls,
+    useGLTF 
+} from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf');
+
     return <>
+
+        <Environment preset="city" />
 
         <color args={[ '#241a1a' ]} attach="background" />
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <PresentationControls
+            global // NOTE drag from any point in the screen
+            rotation={[ 0.13, 0.1, 0 ]}
+            polar={[ -0.4, 0.2 ]}
+            azimuth={[ - 1, 0.75 ]}
+            config={{ mass: 2, tension: 400 }}
+            snap={{ mass: 4, tension: 400 }}
+        >
+            <Float rotationIntensity={ 0.4 }>
+                <rectAreaLight // NOTE simulate the light from the screen
+                    width={ 2.5 }
+                    height={ 1.65 }
+                    intensity={ 65 }
+                    color={ '#ff6900' }
+                    rotation={[ - 0.1, Math.PI, 0 ]}
+                    position={[ 0, 0.55, - 1.15 ]}
+                />
+                <primitive 
+                    object={ computer.scene }
+                    position-y={ - 1.2 }
+                >
+                    <Html
+                        transform
+                        wrapperClass="htmlScreen"
+                        distanceFactor={ 1.17 }
+                        position={[ 0,  1.56, - 1.4 ]} // NOTE relative to the laptop
+                        rotation-x={ -0.256 }
+                    >
+                        <iframe src="https://bruno-simon.com/html" />
+                    </Html>
+                </primitive>
+                <Text
+                    font="./bangers-v20-latin-regular.woff"
+                    fontSize={ 1 }
+                    position={[ 2, 0.75, 0.75 ]}
+                    rotation-y={ - 1.25 }
+                    maxWidth={ 2 }
+                    textAlign="center"
+                >
+                    BRUNO SIMON
+                </Text>
+            </Float>
+        </PresentationControls>
 
+        <ContactShadows
+            position-y={ - 1.4 }
+            opacity={ 0.4 }
+            scale={ 5 }
+            blur={ 2.4 }
+        />
     </>
 }
